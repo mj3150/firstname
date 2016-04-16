633d5246af55130757a5ae6a3b06a5ba06535a2d
Max <mj31508@gmail.com>
2016-04-16 14:56:12 -0400

    age.rb

---
+++ age.rb
@@ -0,0 +1,11 @@
+puts "Hey my man, how old are you?"
+
+age = gets.chomp.to_i
+
+puts "I am #{age}"
+
+puts "So in ten years you will be #{age + 10}"
+puts "In twenty years you will be #{age + 20}"
+puts "In thirty years you will be #{age + 30}"
+puts "In forty years you will be #{age + 40}"
+puts "In fifty years you will be #{age + 50}"
