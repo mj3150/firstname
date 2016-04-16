e6a457c115cf5400b4bb0797447e129b0f3b7908
Max <mj31508@gmail.com>
2016-04-16 15:50:49 -0400

    updated name.rb

--- name.rb
+++ name.rb
@@ -3,3 +3,21 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "So your name is #{name}!! It is very nice to meet you #{name}"
+
+puts "I am going to say your name ten times"
+
+puts "#{name * 10}"
+
+
+
+#or I could do it like this
+puts "I am going to say your name 10 times"
+
+
+
+10.times do 
+
+puts name,  " "
+end
+
+
