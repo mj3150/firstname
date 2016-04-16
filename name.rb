9514b2f661a3a982176871049c9ada91f704283d
Max <mj31508@gmail.com>
2016-04-16 16:31:11 -0400

    updated name.rb

--- name.rb
+++ name.rb
@@ -31,4 +31,12 @@ last = gets.chomp
 
 puts "So your name is #{first} " " #{last}"
 
+#the right answer from launch school
+
+puts "What is your first name?"
+first_name = gets.chomp
+puts "Thank you. What is your last name?"
+last_name = gets.chomp
+puts "Great. So your full name is " + first_name + " " + last_name
+
 
