5e6e5e6a1d08d2bde0f9ad757decbe4cdceaf584
Max <mj31508@gmail.com>
2016-05-04 20:38:30 -0400

    Method question 2

---
+++ ex.rb
@@ -0,0 +1,9 @@
+x = 2 #prints nothing, it just goes to the next line.
+
+puts x = 2 # it puts 2
+
+p name = "Joe" #it puts "Joe"
+
+four = "four" #it puts nothing
+
+print something = "nothing" #it prints the word "nothing" without a line break. I got this question wrong because I thought that it wouldn't have printed anything.
