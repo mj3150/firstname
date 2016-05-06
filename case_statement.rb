03861a5708fbbdcfa0e3dae4a6bac2d786464cad
Max <mj31508@gmail.com>
2016-05-05 21:24:24 -0400

    flow control question 1

---
+++ case_statement.rb
@@ -0,0 +1,49 @@
+# case_statement.rb
+
+a = 5
+
+case a 
+when 5 
+  puts "a is 5"
+
+when 6 
+  puts "a is 6"
+else
+  puts "a is neither 5, nor 6"
+end
+
+# case_statement.rb <-- refactored
+
+a = 5
+ 
+answer = case a
+  when 5
+    "a is 5"
+  when 6
+    "a is 6"
+  else
+    "a is neither 5, nor 6"
+  end
+
+puts answer
+
+# case_statement.rb <-- refactored with no case argument
+
+a = 0
+
+answer = case
+  when a == 5
+    "a is 5"
+  when a == 6
+    "a is 6"
+  else 
+    "a is neither 5, nor 6"
+end
+
+puts answer
+
+if x = 5
+   puts "how can this be true?"
+else
+   puts "it is not true"
+end
---
+++ conditional.rb
@@ -0,0 +1,12 @@
+#flow control question 1
+
+(32 * 4) >= 129 #false#
+
+false != !true #false#
+
+true == 4 #true# #got it wrong. I was under the assumption that positive numbers always equaled true.
+
+false == (847 == '874') #true#
+
+(!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false #I'm not even sure. The ((  ))  confused me
+
