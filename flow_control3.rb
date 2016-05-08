3716476018977d832f7d64645ceb98f3c9b65137
Max <mj31508@gmail.com>
2016-05-08 18:52:03 -0400

    flow control 3

--- flow_control3.rb
+++ flow_control3.rb
@@ -1,19 +1,51 @@
+#1'4' == 4 ? puts("TRUE") : puts("FALSE")
+  #True
+#I am getting a syntax error, unexpected ( arg, expecting keyword_do #or '{' or '('
+ '#4' == 4 ? puts("TRUE") : puts ("FALSE")
+                                  ^
 
-puts "Give me a number between 0 and 100"
 
-a = gets.chomp.to_i
-
-if a == 50..100
-	puts "So your number is #{a}, and that is between 0 and 50"
-
-elsif
-	a <= 50
-
-	puts "#{a} is a pretty low number because it is less than 50"
+#2
+x = 2 
+if ((x * 3) / 2) == (4 + 4 - x - 3)
+   puts "Did you get it right?"
 else
-	puts "#{a} is over 100"
-
-
+   puts "Did you?"
 end
 
-	
+#Answer: Did you get it right?
+
+#3
+ 
+y = 9
+x = 10
+if (x + 1) <= (y)
+   puts "Alright."
+elsif (x + 1) >= (y)
+   puts "Alright now!"
+elsif (y + 1) == x
+   puts "ALRIGHT NOW!"
+else
+   puts "Alrighty!"
+end
+
+#Answer: Alright now!
+
+#4
+
+def math(y,x)
+
+case x
+
+when (x + 1) >= (y)
+	puts "Alright."
+when (x + 1) >= (y)
+	puts "Alright now!"
+else 
+   	puts "Alrighty!"
+end
+end
+
+math(9,10)
+
+#Answer: Alrighty!
